fa6ac558c83c38bc22e9d49e5053f1d933f76e09
Gsancho <luqh555@gmail.com>
2020-06-12 15:32:39 +0200

    Ejercicios

---
+++ Basicos/Numeros/numeros2.php
@@ -0,0 +1,15 @@
+<?php
+
+//gsprogramacion.com
+
+
+
+$potencia = pow(10,5);
+
+echo "10 elevado a 4 = ". $potencia;
+
+echo "<br>";
+
+echo "Raiz cuadrada de 10 elevado a 4 = " . round(sqrt($potencia),3);   //usamos una funcion y como parametro le pasamos el resultado de otra funcion
+
+?>
