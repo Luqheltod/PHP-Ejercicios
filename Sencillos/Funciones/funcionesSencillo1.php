dc239c08a5e03ef2a89293b6e7515df38696ab35
Gsancho <luqh555@gmail.com>
2020-08-31 18:23:24 +0200

    Sencillo Funciones

---
+++ Sencillos/Funciones/funcionesSencillo1.php
@@ -0,0 +1,22 @@
+<?php
+
+
+//- Realiza una funcion a la que se le pasará un array de numeros. La funcion devuelve la media de dichos numeros incluidos en el array.
+
+
+//Recorremos el array y sumamos los valores, despues lo dividemos entre la cantidad de valores que tuviese el array.
+function devuelveMedia($array){
+    
+    $cantidad = count($array);
+    $total = 0;
+    foreach ($array as $valor) {
+        $total +=$valor;;
+    }
+    
+    return $total/$cantidad;
+}
+
+
+echo devuelveMedia([2,2,2,2]);
+
+?> 
